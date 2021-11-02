81dbce20a53a57707d89f7bae2cc742a6f1b19eb
fishming <1661342068@qq.com>
2021-11-02 22:23:26 +0800

    cuda10 docker

--- docker/install/centos_build.sh
+++ docker/install/centos_build.sh
@@ -1,5 +1,5 @@
 yum install -y wget bzip2 expect gcc which numactl-devel numatcl && yum clean all && rm -rf /var/cache/yum/*
 cd /home
-wget -q https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-x86_64.sh
+wget --no-check-certificate -q https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.05-Linux-x86_64.sh
 chmod 777 Anaconda3-2021.05-Linux-x86_64.sh
 
--- docker/install/centos_dgl.sh
+++ docker/install/centos_dgl.sh
@@ -11,8 +11,8 @@ conda config --set show_channel_urls yes
 
 conda create -n pytorch python=3.8
 source activate pytorch
-conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1
-conda install -c dglteam dgl-cuda11.1
+conda install pytorch torchvision torchaudio cudatoolkit=10.2
+conda install -c dglteam dgl-cuda10.2
 conda install -c conda-forge ninja
 pip install auditwheel
 pip install requests
